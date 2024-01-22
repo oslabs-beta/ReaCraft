ad1d1efb9d78d1a1cc5e425c5a0f0b24a539d366
Amir <amirm034@gmail.com>
2024-01-21 22:07:13 -0800

    20240121_Feature/Components_Canvas_2207_Rearrange toolbar icons and update floating button behavior

--- client/src/components/Workspace/WorkspaceRight.jsx
+++ client/src/components/Workspace/WorkspaceRight.jsx
@@ -6,38 +6,32 @@ import ViewCodeButton from '../functionalButtons/ViewCodeButton';
 import { useSelector } from 'react-redux';
 import { convertToTree } from '../../utils/treeNode';
 import { jsxCode } from '../../utils/jsxCode';
-import ViewKeyboardShortcut from '../functionalButtons/ViewKeyboardShortcut';
 import DeleteDesignButton from '../functionalButtons/DeleteDesignButton';
-import UserImageUpload from '../functionalButtons/UserImageUploadButton';
 
 export default function WorkspaceRight({ selectedIdx }) {
   const components = useSelector((state) => state.designV2.components);
-  const { _id } = useSelector((state) => state.designV2);
+  // const { _id } = useSelector((state) => state.designV2);
   const tree = convertToTree(components);
   const code = jsxCode(components, tree);
   return (
     <Stack width='36px' direction='column' gap={2}>
       <Box
         width='36px'
-        height='200px'
+        height='100px'
         sx={{
           display: 'flex',
           flexDirection: 'column',
           justifyContent: 'space-between',
         }}>
-        <ViewKeyboardShortcut
-          sx={{ position: 'absolute', justifySelf: 'end' }}
-        />
-        <UserImageUpload height='64px' />
         <ViewDomTreeButton tree={tree} />
         <ViewCodeButton
           code={code}
           name={selectedIdx !== null ? components[selectedIdx].name : null}
         />
       </Box>
-      <Box maxWidth='false' sx={{ position: 'absolute', top: '50vh' }}>
+      {/* <Box maxWidth='false' sx={{ position: 'absolute', top: '50vh' }}>
         <DeleteDesignButton designId={_id} />
-      </Box>
+      </Box> */}
     </Stack>
   );
 }
