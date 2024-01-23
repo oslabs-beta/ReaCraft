2856d7b69bc28bda8303c69f69b11beeb413be36
Claire H <clairehuang1008@gmail.com>
2024-01-22 19:51:57 -0500

    box-shadow

--- client/src/components/functionalButtons/ZoomSlider.jsx
+++ client/src/components/functionalButtons/ZoomSlider.jsx
@@ -7,6 +7,7 @@ import RemoveCircleRoundedIcon from '@mui/icons-material/RemoveCircleRounded';
 import { Typography } from '@mui/material';
 import { useSelector, useDispatch } from 'react-redux';
 import { setZoom } from '../../utils/reducers/appSlice';
+import RestartAltIcon from '@mui/icons-material/RestartAlt';
 
 export default function ZoomSlider() {
   const zoom = useSelector((state) => state.app.zoom);
@@ -19,6 +20,9 @@ export default function ZoomSlider() {
       <Typography>{zoom}%</Typography>
       <IconButton onClick={() => dispatch(setZoom(zoom + 10))}>
         <AddCircleRoundedIcon />
+      </IconButton>
+      <IconButton onClick={() => dispatch(setZoom(100))} size='small'>
+        <RestartAltIcon />
       </IconButton>
     </Stack>
   );
