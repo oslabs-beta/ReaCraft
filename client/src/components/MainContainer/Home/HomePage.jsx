813cd1384e93193e7a11821ff18d13eac73c8df6
Claire H <clairehuang1008@gmail.com>
2024-01-30 12:40:22 -0500

    get designs requests combined

--- client/src/components/MainContainer/Home/HomePage.jsx
+++ client/src/components/MainContainer/Home/HomePage.jsx
@@ -4,7 +4,6 @@ import PastDesigns from './PastDesigns';
 import { Container } from '@mui/material';
 import HomePageSearch from './HomePageSearch';
 import Box from '@mui/material/Box';
-import CollabDesigns from './CollabDesigns';
 
 export default function Home() {
   const { user } = useAuth();
@@ -15,7 +14,6 @@ export default function Home() {
           {/* conditional rendering of HomPageSearch. if a design is selected, the search bar will not appear in the workspace */}
           <HomePageSearch />
           <PastDesigns />
-          <CollabDesigns />
         </Container>
       </Box>
     );
