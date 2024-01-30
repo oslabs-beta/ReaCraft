497beee27f977b5b266c37347a93f36a6051b52a
Claire H <clairehuang1008@gmail.com>
2024-01-30 16:12:37 -0500

    imageController => ts

--- server/controllers/imageController.ts
+++ server/controllers/imageController.ts
@@ -1,16 +1,22 @@
-const s3 = require('../models/s3Model');
-const websocketController = require ('./websocketController');
-const WebSocket = require('ws');
+import s3 from '../models/s3Model';
+import { getClient } from './websocketController';
+import WebSocket from 'ws';
+import { Request, Response, NextFunction } from 'express';
+import { S3 } from 'aws-sdk';
 
-const uploadImage = (req, res, next) => {
-  // const { userImage } = req.body;
+export const uploadImage = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   console.log('hit uploadImage');
   const { userImage, clientId } = req.body;
   console.log('this is the clientId from req.body', clientId);
+
   if (!userImage) return next();
   if (!clientId) return res.status(404).send('clientId is required');
 
-  const ws = websocketController.getClient(clientId);
+  const ws = getClient(clientId);
   console.log('this is ws from uploadImage', ws);
   if (!ws || ws.readyState !== WebSocket.OPEN) {
     return res.status(404).send('websocket client not found');
@@ -19,7 +25,7 @@ const uploadImage = (req, res, next) => {
   const base64Data = userImage.replace(/^data:image\/\w+;base64,/, '');
   const buffer = Buffer.from(base64Data, 'base64');
 
-  const params = {
+  const params: S3.PutObjectRequest = {
     Bucket: 'reactraft',
     Key: `${Date.now()}.png`,
     Body: buffer,
@@ -28,33 +34,39 @@ const uploadImage = (req, res, next) => {
   };
 
   const upload = s3.upload(params);
-  
-  upload.on('httpUploadProgress', function(evt) {
+
+  upload.on('httpUploadProgress', function (evt) {
     console.log('getting upload progress');
     const progress = Math.round((evt.loaded / evt.total) * 100);
     console.log('this is the progress', progress);
 
-    if (ws.readyState === 1) {
+    if (ws.readyState === WebSocket.OPEN) {
       ws.send(JSON.stringify({ type: 'progressUpdate', progress: progress }));
       console.log('progress update sent to client', clientId);
     } else {
       console.log('websocket not open or does not exist for client', clientId);
     }
   });
-  upload.promise()
-  .then(data => {
-    res.locals.onlineImageUrl = data.Location;
-    next();
-  })
-  .catch(err => {
-    next({
-      log: `Express error handler caught imageController.uploadImage middleware error: ${err}`,
-      message: `Upload image err: ${err}`,
+
+  upload
+    .promise()
+    .then((data) => {
+      res.locals.onlineImageUrl = data.Location;
+      next();
     })
-  })
+    .catch((err) => {
+      next({
+        log: `Express error handler caught imageController.uploadImage middleware error: ${err}`,
+        message: `Upload image err: ${err}`,
+      });
+    });
 };
 
-const deleteImage = (req, res, next) => {
+export const deleteImage = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   let imageToDelete = res.locals.imageToDelete
     ? res.locals.imageToDelete
     : req.body.imageToDelete;
@@ -62,13 +74,15 @@ const deleteImage = (req, res, next) => {
   imageToDelete = Array.isArray(imageToDelete)
     ? imageToDelete
     : [imageToDelete];
-  const params = {
+
+  const params: S3.DeleteObjectsRequest = {
     Bucket: 'reactraft',
     Delete: {
-      Objects: imageToDelete.map((Key) => ({ Key })),
+      Objects: imageToDelete.map((Key: string): { Key: string } => ({ Key })),
       Quiet: false,
     },
   };
+
   return s3
     .deleteObjects(params)
     .promise()
@@ -86,4 +100,4 @@ const deleteImage = (req, res, next) => {
     );
 };
 
-module.exports = { uploadImage, deleteImage };
+export default { uploadImage, deleteImage };
