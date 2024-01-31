4b8de423dbad2ca56c6a4fa6493e03eda31e5402
Claire H <clairehuang1008@gmail.com>
2024-01-31 13:02:19 -0500

    updating tests in ts

--- server/routes/router.ts
+++ server/routes/router.ts
@@ -1,18 +1,22 @@
 import express, { Request, Response } from 'express';
 import path from 'path';
-
-import loginRouter from './loginRouter';
-import signupRouter from './signupRouter';
 import designsRouter from './designsRouter';
 import componentsRouter from './componentsRouter';
 import pageRouter from './pageRouter';
-
 import {
   removeCookie,
   decryptCookie,
   checkCookie,
+  setCookie,
 } from '../controllers/cookieController';
-import { getUser, updateProfilePicture } from '../controllers/userController';
+import {
+  addUser,
+  checkEmail,
+  checkUsername,
+  hashPassword,
+  updateProfilePicture,
+  verifyUser,
+} from '../controllers/userController';
 import { uploadImage, deleteImage } from '../controllers/imageController';
 import { downloadFiles } from '../controllers/fileController';
 
@@ -22,14 +26,25 @@ router.get('/logout', removeCookie, (req: Request, res: Response) =>
   res.status(200).end()
 );
 
-router.use('/login', loginRouter);
-router.use('/signup', signupRouter);
 router.use('/designs', decryptCookie, designsRouter);
 router.use('/components', decryptCookie, componentsRouter);
 router.use('/pages', decryptCookie, pageRouter);
 
-router.get('/user', decryptCookie, getUser, (req: Request, res: Response) =>
+router.get('/user', decryptCookie, (req: Request, res: Response) =>
   res.status(200).json(res.locals.user)
+);
+
+router.post('/login', verifyUser, setCookie, (req: Request, res: Response) =>
+  res.redirect('/home')
+);
+router.post(
+  '/signup',
+  checkEmail,
+  checkUsername,
+  addUser,
+  hashPassword,
+  setCookie,
+  (req: Request, res: Response) => res.redirect('/home')
 );
 
 router.post('/download', downloadFiles);
