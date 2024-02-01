4b63354aa57110212b2193399b98632eb0859c55
Claire H <clairehuang1008@gmail.com>
2024-01-31 22:54:09 -0500

    add console log

--- __tests__/db-connection.test.ts
+++ __tests__/db-connection.test.ts
@@ -1,4 +1,8 @@
 import db from '../server/models/dbModel';
+import dotenv from 'dotenv';
+
+dotenv.config();
+console.log(process.env.PG_URI);
 
 describe('Database Connection', () => {
   it('should connect to the database successfully', async () => {
