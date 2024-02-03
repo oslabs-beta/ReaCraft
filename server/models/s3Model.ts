a3846f1a2b471f78536ed5fc2d24cc0661444408
Claire H <clairehuang1008@gmail.com>
2024-02-03 16:07:50 -0500

    update aws credentials

--- server/models/s3Model.ts
+++ server/models/s3Model.ts
@@ -6,7 +6,7 @@ dotenv.config();
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: 'us-east-1',
+  region: 'us-east-2',
 });
 
 const s3 = new AWS.S3();
